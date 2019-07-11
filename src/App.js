e71af97670357b8c0d884642adef2619ed1e464f
Phu Ly <tug03794@temple.edu>
2019-07-11 02:41:22 -0400

    working searchbar

--- src/App.js
+++ src/App.js
@@ -11,14 +11,20 @@ class App extends Component {
 //
 state = {  
   items:[],
-  query:"https://api.themoviedb.org/3/discover/movie?api_key=fabfadfecd6dcd34f26a4142545c1a29&page=1",
+  page:1,
+  apiUrl:"https://api.themoviedb.org/3/",
+  apiKey:"fabfadfecd6dcd34f26a4142545c1a29",
+  query:"https://api.themoviedb.org/3/discover/movie?api_key=fabfadfecd6dcd34f26a4142545c1a29&page=3",
+  language:"language=en-US",
+  searchMode:"search/movie",
+  searchText:"",
 }
 
 
  // fetch("http://api.themoviedb.org/3/discover/movie?api_key=fabfadfecd6dcd34f26a4142545c1a29&page=2")
- 
   //https://api.themoviedb.org/3/search/movie?api_key=fabfadfecd6dcd34f26a4142545c1a29&language=en-US&query=spider   
   //Search
+
 componentDidMount(){
 
   //const discoverMovie=this.state.apiUrl+this.state.searchMode+"?api_key="+this.state.apiKey+"&"+this.state.language+"&page="+this.state.page+"&query="+this.state.Term;
@@ -34,7 +40,20 @@ componentDidMount(){
   }) 
 }
 
+getSearchText=(text)=>{
+
+const query=this.state.apiUrl+this.state.searchMode+"?api_key="+this.state.apiKey+"&"+this.state.language+"&page="+this.state.page+"&query="+text;
+console.log(query);
+fetch(query)
+  .then(res=>res.json())
+  .then(json=>{
+    this.setState({
+      items:json.results,
+    })
+  }) 
+}
   render() {
+
   //deconstruring the array to get the properties I need.
   const {items}=this.state; 
   const posters=items.map(({id, poster_path,title})=>({id,poster_path,title}));
@@ -43,7 +62,7 @@ componentDidMount(){
       <MuiThemeProvider>
         <div>
             <Header page={this.state.Page}/> 
-            <div className="searchBar"> <SearchBar/>
+            <div className="searchBar"> <SearchBar getSearchText={this.getSearchText}/>
             </div>
 
             <PosterContainer posters={posters}/> 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,29 +4,28 @@ import TextField from 'material-ui/TextField';
 class SearchBar extends Component {
     state = { 
      searchText:"",
-     page:1,
-    apiUrl:"https://api.themoviedb.org/3/",
-    apiKey:"fabfadfecd6dcd34f26a4142545c1a29",
-    query:"https://api.themoviedb.org/3/discover/movie?api_key=fabfadfecd6dcd34f26a4142545c1a29&page=1",
-    language:"language=en-US",
-  searchMode:"search/movie",
- 
     };
-
-
-    onTextChange = e => {
-        this.setState({[e.target.name]:e.target.value});
-    }
+    
     onEnterPress = (e) => {
+        //on Enter Update the query in the state to with the new searchtext
         if (e.key === 'Enter') {
-            console.log(this.props.SearchTerm);
+        this.props.getSearchText(this.state.searchText);
+        this.setState({searchText:""})
         }
       }
-    render() { 
+      onTextChange = e => {
+        this.setState({[e.target.name]:e.target.value});
 
+    }
+    render() { 
         return ( 
             <div>
-            <TextField name="searchText" value={this.state.searchText} onChange={this.onTextChange} onKeyDown={this.onEnterPress} floatingLabelText="Search for Movies And Press Enter" fullWidth={true} autoComplete="true"/>
+            <TextField name="searchText" 
+            value={this.state.searchText} 
+            onChange={this.onTextChange}
+            onKeyDown={this.onEnterPress} 
+            floatingLabelText="Search for Movies And Press Enter" 
+            fullWidth={true} autoComplete="true"/>
             </div>
 );
     }
